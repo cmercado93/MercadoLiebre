7b1c67597d2b371ae8882af1fec400fe2526445f
Carlos Mercado <cmercado93@outlook.com>
2022-09-06 00:50:00 -0300

    Se agregaron los primeros elementos

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req,res) =>{
+    res.sendFile((path.join(__dirname, '/views/home.html')));
+});
+
+app.get('/login', (req,res) =>{
+    res.sendFile((path.join(__dirname, '/views/login.html')));
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile((path.join(__dirname, '/views/register.html')));
+});
+
+app.listen(3000, function() {
+    console.log("Inicio el servidor")
+});
