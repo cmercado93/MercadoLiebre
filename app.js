0dabdf310f451cb45db05029fbd46cb687159b9a
Carlos Mercado <cmercado93@outlook.com>
2022-09-08 00:15:03 -0300

    Cambios

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get('/register', (req,res) =>{
     res.sendFile((path.join(__dirname, '/views/register.html')));
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Inicio el servidor")
 });
